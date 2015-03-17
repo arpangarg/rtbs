58070e4deea385e197c171963404ed5a98bada6c
Arpan Garg <arpangarg93@gmail.com>
2015-03-17 14:53:36 -0400

    add coordinate mapping

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 (function() {
 	/* Canvas */
 
+	//drawFromStream('461:628,438:648,519:618,540:641,630:634,596:638,');
+
 	var canvas = document.getElementById('mycanvas');
 	var ctx = canvas.getContext('2d');
 
-	 
 	ctx.strokeStyle = "black";
 	ctx.lineWidth = '1';
 	ctx.lineCap = ctx.lineJoin = 'round';
@@ -49,8 +50,45 @@
 
     function drawFromStream(message) {
     	if(!message) return;
-    	console.log(message)
+
+    	message = message.split(',');
+    	message.splice(-1, 1);
+
+    	coordinates = [];
+    	messageLength = message.length;
+    	for (var i = 0; i < messageLength; i++) {
+    		coordinate = message[i].split(':');
+    		coordinates.push([
+    			parseFloat(coordinate[0]), parseFloat(coordinate[1])
+    		]);
+    	}
+    	//console.log(coordinates);
+
+    	var homo_mat = [
+    		[-0.001042433086829, -1.689286706623189e-04, 0.999803466389050],
+    		[1.997397384632626e-06, 0.001051583733664, -0.019664922506318],
+    		[7.095613335978725e-08, -6.874736668895402e-07, 0.002024446296485]
+    	];
+
+    	//q = [];
+    	plots = [];
+    	for (var k = 0; k < messageLength; k++) {
+    		var transf = [];
+    		for (var a = 0; a < 3; a++) {
+    			transf.push(
+    				homo_mat[a][0] * coordinates[k][0] + homo_mat[a][1] * coordinates[k][1] + homo_mat[a][2]
+    			);
+    		}
+    		//q.push(transf);
+    		plots.push({x: parseInt(transf[0]/transf[2]), y: parseInt(transf[1]/transf[2])});
+    	}
+
+    	console.log(plots);
+    	if(plots.length > 1) {
+    		drawOnCanvas(plots);
+    	}
 		//if(!message || message.plots.length < 1) return;
+		//console.log(message);
 		//drawOnCanvas(message.plots);
     }
     
